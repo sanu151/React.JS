f6ccf8a6fd3189da7bd6ea36e37d1d65774ddc5d
Supriyo Das <das.supriyo07@gmail.com>
2024-05-19 21:33:44 +0530

    OOP -> React Component

--- React-app-Component/my-app/src/app.js
+++ React-app-Component/my-app/src/app.js
@@ -4,17 +4,15 @@ import Data from "./data/data.json";
 
 const pageHeading = "React Component";
 
-let items = [];
-
-items = Data.map((item) => (
-  <Card cardTitle={item.title} cardBody={item.para} />
-));
-
 const App = () => {
   return (
     <div>
       <h1 className="pageHeading">{pageHeading}</h1>
-      <div className="cardDiv">{items}</div>
+      <div className="cardDiv">
+        {Data.map((item) => (
+          <Card cardTitle={item.title} cardBody={item.para} />
+        ))}
+      </div>
     </div>
   );
 };
